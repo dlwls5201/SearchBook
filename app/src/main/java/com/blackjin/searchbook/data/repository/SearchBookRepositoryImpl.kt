8ab0c4d4f65d244b7ba2610b451e0f74739751b6
dlwls5201 <dlwls5201@naver.com>
2021-03-12 16:39:38 +0900

    withcontext 함수 수정

--- app/src/main/java/com/blackjin/searchbook/data/repository/SearchBookRepositoryImpl.kt
+++ app/src/main/java/com/blackjin/searchbook/data/repository/SearchBookRepositoryImpl.kt
@@ -5,8 +5,6 @@ import com.blackjin.searchbook.ui.model.BookItem
 import com.blackjin.searchbook.ui.model.mapToItem
 import com.blackjin.searchbook.utils.Dlog
 import com.example.toyproject.data.base.BaseResponse
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 
 class SearchBookRepositoryImpl(
@@ -21,32 +19,30 @@ class SearchBookRepositoryImpl(
     }
 
     override suspend fun searchBook(query: String, page: Int, callback: BaseResponse<Triple<Boolean, Int, List<BookItem>>>) {
-        withContext(Dispatchers.Main) {
-            try {
-                callback.onLoading()
-                withContext(Dispatchers.IO) {
-                    val searchResponse = searchApi.searchBook(
-                        target = TARGET,
-                        query = query,
-                        page = page,
-                        size = SIZE
-                    )
+        try {
+            callback.onLoading()
 
-                    val isEnd = searchResponse.meta?.isEnd ?: true
-                    val totalCount = searchResponse.meta?.totalCount ?: 0
-                    Dlog.d("isEnd : $isEnd, totalCount : $totalCount")
+            val searchResponse = searchApi.searchBook(
+                target = TARGET,
+                query = query,
+                page = page,
+                size = SIZE
+            )
 
-                    val items = searchResponse.documents.mapToItem()
-                    callback.onSuccess(Triple(isEnd, totalCount, items))
-                }
-            } catch (e: Exception) {
-                if (e is HttpException) {
-                    callback.onFail("${e.code()} : ${e.message()}")
-                } else {
-                    callback.onError(e)
-                }
+            val isEnd = searchResponse.meta?.isEnd ?: true
+            val totalCount = searchResponse.meta?.totalCount ?: 0
+            Dlog.d("isEnd : $isEnd, totalCount : $totalCount")
+
+            val items = searchResponse.documents.mapToItem()
+            callback.onSuccess(Triple(isEnd, totalCount, items))
+
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                callback.onFail("${e.code()} : ${e.message()}")
+            } else {
+                callback.onError(e)
             }
-            callback.onLoaded()
         }
+        callback.onLoaded()
     }
 }
